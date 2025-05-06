ed968280651522958a91778d679e28679a2cb6dd
Bin-Abdoul <usamaabdulhakeem2006@gmail.com>
2025-05-06 11:46:58 +0100

    All pages done

--- src/MainPages/container.tsx
+++ src/MainPages/container.tsx
@@ -6,7 +6,10 @@ export default function Container() {
   return (
     <div className="sticky top-0 bg-white ">
       <div className="h-18 flex justify-end mx-6 p-5 my-auto">
+        <div className="lg:hidden">
+
         <Nav horizontal="1" />
+        </div>
       </div>
 
       <Separator />
